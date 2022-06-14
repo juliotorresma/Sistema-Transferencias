8e7defbc16d9a8f0c92d1b425678283a6831309d
Omar Mena <omar@getfairplay.com>
2022-06-13 23:01:37 -0500

    final commit
    últimos cambios a proyecto funcional

--- Sistema_Transferencias/bank/wsgi.py
+++ Sistema_Transferencias/bank/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for Sistema_Transferencias project.
+WSGI config for bank project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Sistema_Transferencias.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bank.settings')
 
 application = get_wsgi_application()
